64d3063fb39ce2284416555906f3e50d9cfd355f
Glen <glen15@naver.com>
2021-06-15 20:47:56 +0900

    #2 Setting Typescript Up

---
+++ index.ts
@@ -0,0 +1,13 @@
+// 어떤 종류의 변수와 데이터인지 설정을 해줘야한다는 것
+
+const name = "glen",
+    age = 31,
+    gender = "male";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
