639c66511e9383ab2cb64f461acd41f73d0fc5dc
Glen <glen15@naver.com>
2021-06-15 22:30:09 +0900

    interfaces on ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
- // 어떤 종류의 변수와 데이터인지 설정을 해줘야한다는 것
- 
- const sayHi = (name:string, age:number, gender:string): string => {
-     return `Hello ${name}, you are ${age}, you are a ${gender}!`
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+};
+
+const person = {
+     name: "glen",
+     age: 31,
+     gender: "male"
  };
  
- console.log(sayHi("Glen", 31, "male"));
- // 만약 여기서 인자를 두개받게 못받으면 에러가 난다. 근데 js는 그런거없어! ts가 더 안정성을 준다는 것!
+ const sayHi = (person: Human): string => {
+     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+ };
+ 
+ console.log(sayHi(person));
 
 
  export {};
