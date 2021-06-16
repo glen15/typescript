6d58cdef4f7ea3127c4e8b9f1edcd35c38eac2c9
Glen <glen15@naver.com>
2021-06-16 19:46:02 +0900

    Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
-};
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
 
-const person = {
-     name: "glen",
-     age: 31,
-     gender: "male"
- };
- 
+const yuni = new Human("yuni", 31, "female"); 
+
  const sayHi = (person: Human): string => {
      return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
  };
  
- console.log(sayHi(person));
+ console.log(sayHi(yuni));
 
 
  export {};
